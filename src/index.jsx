43032b9c51e05c9fc6a5c36c02e8a20f24cc91b6
Shuwen Li <swli@CNswli.local>
2016-01-14 23:42:56 +0800

    Shuwen:Succesful Hello World

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import HelloWorld from './hello-world.jsx'
+
+ReactDom.render(
+    <HelloWorld />,
+    document.getElementById("container")
+);
